9507e82ef0a45e7a8dcce38c9338baadecf2cd83
Bobby Hewitt <bobby.hewitt@w12studios.com>
2018-10-18 12:41:15 +0100

    Updates

---
+++ precache-manifest.d89d28a52dcd30f03b1c72f1dc575ae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a6a71d35c1fbb3241cd8",
+    "url": "/tictactoe/static/js/runtime~main.a6a71d35.js"
+  },
+  {
+    "revision": "6adb8e17d04d0291fc5e",
+    "url": "/tictactoe/static/js/main.6adb8e17.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/tictactoe/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "6adb8e17d04d0291fc5e",
+    "url": "/tictactoe/static/css/main.e27b8266.chunk.css"
+  },
+  {
+    "revision": "56ff9fed7caac9c4bcc938cb5eef895a",
+    "url": "/tictactoe/index.html"
+  }
+];
